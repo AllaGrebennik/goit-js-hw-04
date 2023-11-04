e0259f99d078b86ca3b4490b84d0e0cb06530bfd
AllaGrebennik <grebennik.alla@gmail.com>
2023-11-04 20:48:07 +0200

    js-hw-04

--- task-1.js
+++ task-1.js
@@ -1,11 +1,13 @@
 'use strict';
 
-function slugify(title='') {
-    const slug = title.toLowerCase().split(" ");
-    return slug.join("-");
-}
+function isEnoughCapacity(products, containerSize) {
+    let count = 0;
+    for (const prodact of Object.values(products))
+        count += prodact;
+    return count <= containerSize ? true : false;
+};
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+console.log(  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
